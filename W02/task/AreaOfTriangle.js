09c959108f67860587c69bf777cc43ca524640a3
Osamu Mitsuishi <mitsuishi_3214036@icloud.com>
2022-04-13 15:42:49 +0900

    update w02

--- W02/task/AreaOfTriangle.js
+++ W02/task/AreaOfTriangle.js
@@ -1,6 +1,7 @@
 function AreaOfTriangle(v1, v2, v3) {
   var v12 = new Vec3(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
   var v13 = new Vec3(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+
   console.log("v12 = (%f, %f, %f)", v12.x, v12.y, v12.z);
   console.log("v13 = (%f, %f, %f)", v13.x, v13.y, v13.z);
 
