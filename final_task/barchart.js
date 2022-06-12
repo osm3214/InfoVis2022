0d897d38e0bb473a6bdfb1275e141ccb4e8ecc03
Osamu Mitsuishi <mitsuishi_3214036@icloud.com>
2022-06-12 11:58:00 +0900

    add final task data

--- final_task/barchart.js
+++ final_task/barchart.js
@@ -1,25 +1,3 @@
-d3.csv("https://osm3214.github.io/InfoVis2022/W08/task/w08_task01.csv")
-    .then(data => {
-        data.forEach(d => { d.value = +d.value; });
-
-        var config = {
-            parent: '#drawing_region',
-            width: 256,
-            height: 128,
-            margin: { top: 10, right: 10, bottom: 20, left: 60 }
-        };
-
-        const barchart = new BarChart(config, data);
-        barchart.update();
-
-        d3.select('#reverse').on('click', d => { barchart.data.reverse(); barchart.update(); });
-        d3.select('#descend').on('click', d => { barchart.data.sort((a, b) => b.value - a.value); barchart.update(); });
-        d3.select('#ascend').on('click', d => { barchart.data.sort((a, b) => a.value - b.value); barchart.update(); });
-    })
-    .catch(error => {
-        console.log(error);
-    });
-
 class BarChart {
     constructor(config, data) {
         this.config = {
@@ -27,7 +5,7 @@ class BarChart {
             width: config.width || 256,
             height: config.height || 128,
             margin: config.margin || { top: 10, right: 10, bottom: 20, left: 60 },
-            inner_margin: config.inner_margin || { top: 10, right: 10, bottom: 10, left: 0 },
+            inner_margin: config.inner_margin || { top: 10, right: 0, bottom: 0, left: 0 },
             duration: config.duration || 1000
         }
         this.data = data;
@@ -48,33 +26,30 @@ class BarChart {
         self.inner_width = self.config.width - self.config.margin.left - self.config.margin.right;
         self.inner_height = self.config.height - self.config.margin.top - self.config.margin.bottom;
 
-        self.xscale = d3.scaleLinear()
-            .range([0, self.inner_width]);
+        self.xscale = d3.scaleBand()
+            .range([0, self.inner_width])
+            .padding(0.05);
 
-        self.yscale = d3.scaleBand()
-            .range([0, self.inner_height])
-            .paddingInner(.1)
-            .paddingOuter(.2);
+        self.yscale = d3.scaleLinear()
+            .range([self.inner_height, 0]);
 
         self.xaxis = d3.axisBottom(self.xscale)
-            .ticks(5)
-            .tickSizeOuter(0);
-
+            .ticks(12)
+            .tickValues(["01/01", "02/01", "03/01", "04/01", "05/01", "06/01", "07/01", "08/01", "09/01", "10/01", "11/01", "12/01"]);
         self.yaxis = d3.axisLeft(self.yscale)
-            .tickSizeOuter(0);
+            .ticks(4);
 
         self.xaxis_group = self.chart.append('g')
-            .attr('transform', `translate(0, ${self.inner_height})`)
-
-        self.yaxis_group = self.chart.append('g')
+            .attr('transform', `translate(0, ${self.inner_height})`);
+        self.yaxis_group = self.chart.append('g');
     }
 
     update() {
         let self = this;
 
-        self.xmax = d3.max(self.data, d => d.value);
-        self.xscale.domain([0 - self.config.inner_margin.left, self.xmax + self.config.inner_margin.right]);
-        self.yscale.domain(self.data.map(d => d.label));
+        self.xscale.domain(self.data.map(d => d.date));
+        self.yscale.domain([0, d3.max(self.data, d => d.value) + self.config.inner_margin.top]);
+        console.log(d3.max(self.data, d => d.value))
 
         self.render();
     }
@@ -82,38 +57,29 @@ class BarChart {
     render() {
         let self = this;
 
-        self.chart.selectAll("rect")
+        self.chart.selectAll(".bar")
             .data(self.data)
-            .join("rect")
-            .transition().duration(self.config.duration)
-            .attr("x", 0)
-            .attr("y", d => self.yscale(d.label))
-            .attr("width", d => self.xscale(d.value))
-            .attr("height", self.yscale.bandwidth())
-            .attr("fill", function (d) { return d.value == self.xmax ? "crimson" : "steelblue" });
+            .enter()
+            .append("rect")
+            .attr("class", "bar")
+            .attr("x", d => self.xscale(d.date))
+            .attr("y", d => self.yscale(d.value))
+            .attr("width", self.xscale.bandwidth())
+            .attr("height", d => (self.inner_height - self.yscale(d.value)));
 
-        self.chart.selectAll("label")
-            .data(self.data)
-            .join("text")
-            .transition()
-            .text(d => d.value)
-            .attr("class", "label")
-            .attr("x", d => self.xscale(d.value) - 12)
-            .attr("y", d => self.yscale(d.label) + 12)
+        self.chart.selectAll("text")
+            .append("text")
+            .attr("x", 0)
+            .attr("y", self.config.height / 2)
             .attr("text-anchor", "middle")
             .attr("font-size", 12)
-            .attr("fill", "white")
-            .attr("font-family", "sans-serif");
+            .attr("font-family", "sans-serif")
+            .text("# of new cases");
 
         self.xaxis_group
             .call(self.xaxis);
 
         self.yaxis_group
             .call(self.yaxis);
-    }
-
-    reverse() {
-        let self = this;
-        self.data.reverse();
     }
 }
